5ee4a313d64196f57204b7683bb879fc676315f5
ignacio086 <ignaarigomez@gmail.com>
2024-06-24 20:52:33 -0300

    template MonorepoNextExpress

--- monorepoExpNext.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pnpm init && touch pnpm-workspace.yaml && touch tsconfig.json && mkdir apps && cd apps && mkdir back && cd back && pnpm init && pnpm install express && pnpm i -D typescript dotenv @types/express @types/dotenv zod @trpc/server && cd .. && pnpx create-next-app@latest --no-git && cd front && pnpm i @trpc/server @trpc/client zod && cd .. && cd ..
-exit
